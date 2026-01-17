915c139c5fa1ef706856e2954f4576d53421857d
Bhuneshvar <imankitkalirawana@gmail.com>
2026-01-17 11:30:40 +0530

    feat(activity): add endpoint to retrieve activity logs by stakeholder

--- src/modules/common/activity/activity.controller.ts
+++ src/modules/common/activity/activity.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Query, ParseEnumPipe } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { ActivityLogService } from './services/activity-log.service';
 import { EntityType } from './enums/entity-type.enum';
+import { Request } from 'express';
 
 @Controller('activity')
 export class ActivityController {
@@ -8,9 +9,17 @@ export class ActivityController {
 
   @Get('logs')
   async getActivityLogs(
-    @Query('type', new ParseEnumPipe(EntityType)) type: EntityType,
+    @Query('type') type: EntityType,
     @Query('id') id: string,
   ) {
     return this.activityLogService.getActivityLogsByEntity(type, id);
+  }
+
+  //   get by me
+  @Get('logs/my')
+  async getActivityLogsByStakeholder(@Req() req: Request) {
+    return this.activityLogService.getActivityLogsByStakeholder(
+      req.user?.userId,
+    );
   }
 }
